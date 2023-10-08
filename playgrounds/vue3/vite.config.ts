c32928cc8c26764caea423b7fa1508ca969d4111
KID-joker <kid_joker@126.com>
2023-10-08 19:59:46 +0800

    fix: playground support vue2

--- playgrounds/vue3/vite.config.ts
+++ playgrounds/vue3/vite.config.ts
@@ -1,5 +1,4 @@
-import { URL, fileURLToPath } from 'node:url'
-
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,7 +9,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue-masonry-grid': resolve(__dirname, '../../src'),
     },
   },
 })
